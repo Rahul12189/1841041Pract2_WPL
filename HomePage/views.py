f24919495ae539cbd301c6b2c7998f15bf7b1853
@Rahul12189 <srkadam0699@gmail.com>
2021-04-06 09:22:35 +0530

    First commit

--- HomePage/views.py
+++ HomePage/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	return HttpResponse(" Hello World")
+	return HttpResponse(" Hello, This is Rahul Kadam From T3 Batch with PRN 1841041.")
